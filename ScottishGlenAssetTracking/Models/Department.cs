e8dae79c6fdeb70543ac268891f0fd97cf538be9
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-10-22 12:59:01 +0100

    Added properties to Department.cs.

--- ScottishGlenAssetTracking/Models/Department.cs
+++ ScottishGlenAssetTracking/Models/Department.cs
@@ -8,5 +8,24 @@ namespace ScottishGlenAssetTracking.Models
 {
     public class Department
     {
+        private int _id;
+        private string _name;
+        private List<Employee> _employees;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public List<Employee> Employees
+        {
+            get { return _employees; }
+            set { _employees = value; }
+        }
     }
 }
