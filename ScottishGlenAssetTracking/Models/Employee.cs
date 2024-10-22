701fa6fb7097d99443fd29c919a0c7cfddfb566b
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-10-22 13:00:57 +0100

    Added properties to Employee.cs.

--- ScottishGlenAssetTracking/Models/Employee.cs
+++ ScottishGlenAssetTracking/Models/Employee.cs
@@ -8,5 +8,42 @@ namespace ScottishGlenAssetTracking.Models
 {
     public class Employee
     {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private Department _department;
+        private List<Asset> _assets;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public Department Department
+        {
+            get { return _department; }
+            set { _department = value; }
+        }
+        public List<Asset> Assets
+        {
+            get { return _assets; }
+            set { _assets = value; }
+        }
     }
 }
