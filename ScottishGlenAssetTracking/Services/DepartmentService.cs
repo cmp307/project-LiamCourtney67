b7e0839553f968e0014c70f85a47532f9b079283
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-10-26 20:45:54 +0100

    Added CRUD methods to AssetService.cs, EmployeeService.cs, and DepartmentService.cs.

--- ScottishGlenAssetTracking/Services/DepartmentService.cs
+++ ScottishGlenAssetTracking/Services/DepartmentService.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using ScottishGlenAssetTracking.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScottishGlenAssetTracking.Data;
 
 namespace ScottishGlenAssetTracking.Services
 {
     public class DepartmentService
     {
+        public Department GetDepartment(int id)
+        {
+            using (var context = new ScottishGlenContext())
+            {
+                return context.Departments.Find(id);
+            }
+        }
+
+        public List<Department> GetDepartments()
+        {
+            using (var context = new ScottishGlenContext())
+            {
+                return context.Departments.ToList();
+            }
+        }
     }
 }
