c0be4bb4e0d8f5894898b9d9a3ac3ca064338d46
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-10-31 23:02:38 +0000

    Added ViewAssetViewModel.cs and moved functionality from ViewAsset.cs view.

---
+++ ScottishGlenAssetTracking/ViewModels/ViewAssetViewModel.cs
@@ -0,0 +1,170 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using ScottishGlenAssetTracking.Models;
+using ScottishGlenAssetTracking.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottishGlenAssetTracking.ViewModels
+{
+    public partial class ViewAssetViewModel : ObservableObject
+    {
+        private readonly DepartmentService _departmentService;
+        private readonly EmployeeService _employeeService;
+        private readonly AssetService _assetService;
+
+        public ViewAssetViewModel()
+        {
+            // Initialize services
+            _departmentService = new DepartmentService();
+            _employeeService = new EmployeeService();
+            _assetService = new AssetService();
+
+            // Load departments
+            Departments = new ObservableCollection<Department>(_departmentService.GetDepartments());
+
+            // Initialize collections
+            Employees = new ObservableCollection<Employee>();
+
+            // Initialize commands
+            SelectDepartmentCommand = new RelayCommand(LoadEmployees);
+            SelectEmployeeCommand = new RelayCommand(LoadAssets);
+            SelectAssetCommand = new RelayCommand(PopulateAssetDetails);
+
+            DeleteAssetCommand = new RelayCommand(DeleteAsset);
+            UpdateAssetCommand = new RelayCommand(UpdateAsset);
+
+            ChangeViewToEditCommand = new RelayCommand(ChangeViewToEdit);
+            ChangeViewToViewCommand = new RelayCommand(ChangeViewToView);
+        }
+
+        // Collections
+        public ObservableCollection<Department> Departments { get; }
+        public ObservableCollection<Employee> Employees { get; private set; }
+
+        public ObservableCollection<Asset> Assets { get; private set; }
+
+        // Properties
+        [ObservableProperty]
+        private Department selectedDepartment;
+
+        [ObservableProperty]
+        private Employee selectedEmployee;
+
+        [ObservableProperty]
+        private Asset selectedAsset;
+
+        [ObservableProperty]
+        private string statusMessage;
+
+        // Visibility properties
+        [ObservableProperty]
+        private Visibility statusVisibility = Visibility.Collapsed;
+
+        [ObservableProperty]
+        private Visibility viewAssetViewVisibility = Visibility.Collapsed;
+
+        [ObservableProperty]
+        private Visibility editAssetViewVisibility = Visibility.Collapsed;
+
+        // Commands
+        public RelayCommand SelectDepartmentCommand { get; }
+        public RelayCommand SelectEmployeeCommand { get; }
+        public RelayCommand SelectAssetCommand { get; }
+        public RelayCommand DeleteAssetCommand { get; }
+        public RelayCommand UpdateAssetCommand { get; }
+        public RelayCommand ChangeViewToEditCommand { get; }
+        public RelayCommand ChangeViewToViewCommand { get; }
+
+        private void LoadEmployees()
+        {
+            if (SelectedDepartment != null)
+            {
+                var employees = _employeeService.GetEmployees(SelectedDepartment.Id);
+                Employees = new ObservableCollection<Employee>(employees);
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        private void LoadAssets()
+        {
+            if (SelectedEmployee != null)
+            {
+                var assets = _assetService.GetAssets(SelectedEmployee.Id);
+                Assets = new ObservableCollection<Asset>(assets);
+                OnPropertyChanged(nameof(Assets));
+            }
+        }
+
+        private void PopulateAssetDetails()
+        {
+            if (SelectedAsset != null)
+            {
+                selectedAsset.Employee = Employees.FirstOrDefault(e => e.Id == SelectedAsset.Employee.Id);
+                OnPropertyChanged(nameof(SelectedAsset));
+                StatusVisibility = Visibility.Collapsed;
+                StatusMessage = string.Empty;
+                ChangeViewToView();
+            }
+        }
+
+        private void DeleteAsset()
+        {
+            if (SelectedAsset != null)
+            {
+                _assetService.DeleteAsset(SelectedAsset.Id);
+                StatusVisibility = Visibility.Visible;
+                StatusMessage = "Asset Deleted";
+                LoadAssets();
+                ChangeViewToView();
+            }
+            else
+            {
+                StatusMessage = "No Asset Selected";
+            }
+        }
+
+        private void UpdateAsset()
+        {
+            if (SelectedAsset != null)
+            {
+                _assetService.UpdateAsset(SelectedAsset);
+                OnPropertyChanged(nameof(SelectedAsset));
+
+                int selectedEmployeeId = SelectedAsset.Employee.Id;
+                int selectedAssetId = SelectedAsset.Id;
+
+                SelectedDepartment = Departments.FirstOrDefault(d => d.Id == SelectedAsset.Employee.Department.Id);
+                LoadEmployees();
+                SelectedEmployee = Employees.FirstOrDefault(e => e.Id == selectedEmployeeId);
+                LoadAssets();
+                SelectedAsset = Assets.FirstOrDefault(a => a.Id == selectedAssetId);
+
+                StatusVisibility = Visibility.Visible;
+                StatusMessage = "Asset Updated";
+
+                ChangeViewToView();
+            }
+        }
+
+        private void ChangeViewToEdit()
+        {
+            if (SelectedAsset != null)
+            {
+                ViewAssetViewVisibility = Visibility.Collapsed;
+                EditAssetViewVisibility = Visibility.Visible;
+            }
+        }
+
+        private void ChangeViewToView()
+        {
+            EditAssetViewVisibility = Visibility.Collapsed;
+            ViewAssetViewVisibility = Visibility.Visible;
+        }
+    }
+}
--- ScottishGlenAssetTracking/Views/Asset/ViewAsset.xaml.cs
+++ ScottishGlenAssetTracking/Views/Asset/ViewAsset.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using ScottishGlenAssetTracking.Models;
 using ScottishGlenAssetTracking.Services;
+using ScottishGlenAssetTracking.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -30,160 +31,35 @@ namespace ScottishGlenAssetTracking.Views.Asset
         public ViewAsset()
         {
             this.InitializeComponent();
-
-            List<Department> departments = new DepartmentService().GetDepartments();
-
-            DepartmentSelect.ItemsSource = departments;
-
-            List<Department> departmentsWithoutTemp = new List<Department>(departments);
-            departmentsWithoutTemp.Remove(departmentsWithoutTemp.Find(d => d.Name == "Assets without Employee"));
-
-            AssetDepartmentSelect.ItemsSource = departmentsWithoutTemp;
+            this.DataContext = new ViewAssetViewModel();
         }
 
         private void DepartmentSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (DepartmentSelect.SelectedItem != null)
+            if (DataContext is ViewAssetViewModel viewModel)
             {
-                List<Models.Employee> employees = new EmployeeService().GetEmployees(((Department)DepartmentSelect.SelectedItem).Id);
-                EmployeeSelect.ItemsSource = employees;
-                AssetEmployeeSelect.ItemsSource = employees;
+                viewModel.SelectDepartmentCommand.Execute(null);
             }
         }
 
         private void EmployeeSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (EmployeeSelect.SelectedItem != null)
-            {
-                AssetSelect.ItemsSource = new AssetService().GetAssets(((Models.Employee)EmployeeSelect.SelectedItem).Id);
+            if (DataContext is ViewAssetViewModel viewModel) {
+                viewModel.SelectEmployeeCommand.Execute(null);
             }
         }
 
         private void AssetSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            PopulateAssetDetails();
-            ViewAssetView.Visibility = Visibility.Visible;
-        }
-
-        private void PopulateAssetDetails()
-        {
-            var asset = (Models.Asset)AssetSelect.SelectedItem;
-            if (asset != null)
+            if (DataContext is ViewAssetViewModel viewModel)
             {
-                AssetName.Text = $"Name: {asset.Name}";
-                AssetModel.Text = $"Model: {asset.Model}";
-                AssetManufacturer.Text = $"Manufacturer: {asset.Manufacturer}";
-                AssetType.Text = $"Type: {asset.Type}";
-                AssetIpAddress.Text = $"IP Address: {asset.IpAddress}";
-                AssetPurchaseDate.Text = $"Purchase Date: {asset.PurchaseDate}";
-                AssetNotes.Text = $"Notes: {asset.Notes}";
-                AssetDepartment.Text = $"Department: {asset.Employee.Department.Name}";
-                AssetEmployee.Text = $"Employee: {asset.Employee.FirstName} {asset.Employee.LastName}";
-
-                _selectedAsset = asset;
+                viewModel.SelectAssetCommand.Execute(null);
             }
-            else
-            {
-                AssetName.Text = "Name: ";
-                AssetModel.Text = "Model: ";
-                AssetManufacturer.Text = "Manufacturer: ";
-                AssetType.Text = "Type: ";
-                AssetIpAddress.Text = "IP Address: ";
-                AssetPurchaseDate.Text = "Purchase Date: ";
-                AssetNotes.Text = "Notes: ";
-                AssetDepartment.Text = "Department: ";
-                AssetEmployee.Text = "Employee: ";
-
-                _selectedAsset = null;
-            }
-            PopulateInputDetails(asset);
-        }
-
-        private void PopulateInputDetails(Models.Asset asset)
-        {
-            if (asset != null)
-            {
-                AssetNameInput.Text = asset.Name;
-                AssetModelInput.Text = asset.Model;
-                AssetManufacturerInput.Text = asset.Manufacturer;
-                AssetTypeInput.Text = asset.Type;
-                AssetIpAddressInput.Text = asset.IpAddress;
-                AssetPurchaseDateInput.Date = (DateTime)asset.PurchaseDate;
-                AssetNotesInput.Text = asset.Notes;
-
-                // Set the asset department in the dropdown.
-                AssetDepartmentSelect.SelectedItem = ((List<Department>)AssetDepartmentSelect.ItemsSource)
-                    .FirstOrDefault(d => d.Id == asset.Employee.Department.Id);
-
-                // Set the asset employee in the dropdown.
-                AssetEmployeeSelect.SelectedItem = ((List<Models.Employee>)AssetEmployeeSelect.ItemsSource)
-                    .FirstOrDefault(e => e.Id == asset.Employee.Id);
-            }
-            else
-            {
-                AssetNameInput.Text = string.Empty;
-                AssetModelInput.Text = string.Empty;
-                AssetManufacturerInput.Text = string.Empty;
-                AssetTypeInput.Text = string.Empty;
-                AssetIpAddressInput.Text = string.Empty;
-                AssetPurchaseDateInput.Date = DateTime.Now;
-                AssetNotesInput.Text = string.Empty;
-                AssetDepartmentSelect.SelectedItem = null;
-                AssetEmployeeSelect.SelectedItem = null;
-            }
-        }
-
-        private void DeleteAssetButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_selectedAsset != null)
-            {
-                new AssetService().DeleteAsset(_selectedAsset.Id);
-                Status.Text = "Asset Deleted";
-                PopulateAssetDetails();
-                AssetSelect.ItemsSource = new AssetService().GetAssets(((Models.Employee)EmployeeSelect.SelectedItem).Id);
-            }
-            else
-            {
-                Status.Text = "No Asset Selected";
-            }
-        }
-
-        private void EditAssetButton_Click(object sender, RoutedEventArgs e)
-        {
-            ViewAssetView.Visibility = Visibility.Collapsed;
-            EditAssetView.Visibility = Visibility.Visible;
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            EditAssetView.Visibility = Visibility.Collapsed;
-            ViewAssetView.Visibility = Visibility.Visible;
-        }
-
-        private void UpdateAssetButton_Click(object sender, RoutedEventArgs e)
-        {
-            _selectedAsset.Name = AssetNameInput.Text;
-            _selectedAsset.Model = AssetModelInput.Text;
-            _selectedAsset.Manufacturer = AssetManufacturerInput.Text;
-            _selectedAsset.Type = AssetTypeInput.Text;
-            _selectedAsset.IpAddress = AssetIpAddressInput.Text;
-            _selectedAsset.PurchaseDate = AssetPurchaseDateInput.Date.Date;
-            _selectedAsset.Notes = AssetNotesInput.Text;
-            _selectedAsset.Employee = (Models.Employee)AssetEmployeeSelect.SelectedItem;
-            new AssetService().UpdateAsset(_selectedAsset);
-            Status.Text = "Asset Updated";
-            PopulateAssetDetails();
-            EditAssetView.Visibility = Visibility.Collapsed;
-            ViewAssetView.Visibility = Visibility.Visible;
-            PopulateInputDetails(_selectedAsset);
         }
 
         private void AssetDepartmentSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (AssetDepartmentSelect.SelectedItem != null)
-            {
-                AssetEmployeeSelect.ItemsSource = new EmployeeService().GetEmployees(((Department)AssetDepartmentSelect.SelectedItem).Id);
-            }
+
         }
 
         private void AssetEmployeeSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
