7a5ee318753fc5b5bbc9919d61641800a66e95a9
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-11-21 20:44:39 +0000

    Initial commit with properties for SoftwareAsset model.

---
+++ ScottishGlenAssetTracking/Models/SoftwareAsset.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScottishGlenAssetTracking.Models
+{
+    /// <summary>
+    /// Model class for the SoftwareAsset entity.
+    /// </summary>
+    public class SoftwareAsset
+    {
+        // Private fields for the SoftwareAsset entity.
+        private int _id;
+        private string _name;
+        private string _version;
+        private string _manufacturer;
+
+        /// <summary>
+        /// Id property for the SoftwareAsset entity.
+        /// </summary>
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        /// <summary>
+        /// Name property for the SoftwareAsset entity.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        /// <summary>
+        /// Version property for the SoftwareAsset entity.
+        /// </summary>
+        public string Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+
+        /// <summary>
+        /// Manufacturer property for the SoftwareAsset entity.
+        /// </summary>
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+            set { _manufacturer = value; }
+        }
+    }
+}
