27ded508653b19aeb74dc9756ccd321dfe3a8ff3
Liam Courtney <70069335+LiamCourtney67@users.noreply.github.com>
2024-11-28 13:46:53 +0000

    Added error handling to the Login and Register views and view models.

--- ScottishGlenAssetTracking/ViewModels/LoginViewModel.cs
+++ ScottishGlenAssetTracking/ViewModels/LoginViewModel.cs
@@ -2,6 +2,8 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
+using MySqlConnector;
+using ScottishGlenAssetTracking.Data;
 using ScottishGlenAssetTracking.Models;
 using ScottishGlenAssetTracking.Services;
 using ScottishGlenAssetTracking.Views.Account;
@@ -9,21 +11,33 @@ using ScottishGlenAssetTracking.Views.HardwareAsset;
 using ScottishGlenAssetTracking.Views.Portals;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ScottishGlenAssetTracking.ViewModels
 {
+    /// <summary>
+    /// Partial class for the LoginViewModel using the ObservableObject class.
+    /// </summary>
     public partial class LoginViewModel : ObservableObject
     {
+        // Private field for the AccountService.
         private readonly AccountService _accountService;
 
+        /// <summary>
+        /// Constructor for the LoginViewModel class using the AccountService with dependency injection.
+        /// </summary>
+        /// <param name="accountService">AccountService from dependency injection.</param>
         public LoginViewModel(AccountService accountService)
         {
+            // Set the account service.
             _accountService = accountService;
         }
 
+        // Properties.
         [ObservableProperty]
         private string email;
 
@@ -33,60 +47,120 @@ namespace ScottishGlenAssetTracking.ViewModels
         [ObservableProperty]
         private string statusMessage;
 
+        // Visibility properties.
         [ObservableProperty]
         private Visibility statusVisibility = Visibility.Visible;
 
+        // Commands
+
+        /// <summary>
+        /// Command to log in the user.
+        /// </summary>
         [RelayCommand]
         public void Login()
         {
-            if (App.AppHost.Services.GetRequiredService<AccountManager>().Login(Email, Password))
+            // Check if the email and password are empty or null.
+            if (Email == string.Empty || Email == null || Password == string.Empty || Password == null)
             {
-                // Reset the email and password properties.
-                Email = string.Empty;
-                Password = string.Empty;
-
-                NavigateToPortal();
+                SetStatusMessage("Please fill in all fields.");
+                return;
             }
-            else
+
+            // Attempt to log in the user, catching any exceptions that may occur.
+            try
+            { 
+                if (App.AppHost.Services.GetRequiredService<AccountManager>().Login(Email, Password))
+                {
+                    ResetProperties();
+                    NavigateToPortal();
+                }
+                else
+                {
+                    Password = string.Empty;
+                    SetStatusMessage("Login failed.");
+                }
+            }
+            catch (AuthenticationException ex)
             {
-                StatusMessage = "Login failed";
-                StatusVisibility = Visibility.Visible;
+                Password = string.Empty;
+                SetStatusMessage(ex.Message);
+            }
+            catch (MySqlException ex)
+            {
+                Password = string.Empty;
+                SetStatusMessage("There was an issue connecting to the database. Please try again later.");
+            }
+            catch (Exception ex)
+            {
+                Password = string.Empty;
+                SetStatusMessage("An unexpected error occurred. Please try again later.");
             }
         }
 
+        /// <summary>
+        /// Command to navigate to the register page.
+        /// </summary>
         [RelayCommand]
         public void NavigateToRegister()
         {
-            // Reset the email and password properties.
-            Email = string.Empty;
-            Password = string.Empty;
+            ResetProperties();
 
             var registerPage = App.AppHost.Services.GetRequiredService<Register>();
             MainWindow.Frame.Navigate(registerPage.GetType());
         }
 
+        /// <summary>
+        /// Navigates to the appropriate portal based on the account type.
+        /// </summary>
         private void NavigateToPortal()
         {
+            // Get the current account from the AccountManager.
             Account account = App.AppHost.Services.GetRequiredService<AccountManager>().CurrentAccount;
 
             if (account == null)
             {
                 return;
             }
-            else if (account.IsAdmin) {
+            // Admin portal.
+            else if (account.IsAdmin)
+            {
                 var adminPortal = App.AppHost.Services.GetRequiredService<AdminPortal>();
                 MainWindow.Frame.Navigate(adminPortal.GetType());
             }
+            // Employee portal.
             else if (account.Employee != null)
             {
                 var employeePortal = App.AppHost.Services.GetRequiredService<EmployeePortal>();
                 MainWindow.Frame.Navigate(employeePortal.GetType());
             }
+            // New user portal.
             else
             {
                 var newUserPortal = App.AppHost.Services.GetRequiredService<NewUserPortal>();
                 MainWindow.Frame.Navigate(newUserPortal.GetType());
             }
+        }
+
+        /// <summary>
+        /// Helper method to reset the properties of the view model.
+        /// </summary>
+        private void ResetProperties()
+        {
+            // Reset the email, password, and status message properties.
+            Email = string.Empty;
+            Password = string.Empty;
+            StatusMessage = string.Empty;
+        }
+
+        /// <summary>
+        /// Helper method to set the status message and make the status message visible.
+        /// </summary>
+        /// <param name="message">Message to be displayed.</param>
+        private void SetStatusMessage(string message)
+        {
+            // Set the status message and make the status message visible.
+            StatusMessage = message;
+            StatusVisibility = Visibility.Visible;
         }
     }
 }
--- ScottishGlenAssetTracking/ViewModels/RegisterViewModel.cs
+++ ScottishGlenAssetTracking/ViewModels/RegisterViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
+using MySqlConnector;
 using ScottishGlenAssetTracking.Models;
 using ScottishGlenAssetTracking.Services;
 using ScottishGlenAssetTracking.Views.Account;
@@ -13,21 +14,33 @@ using System.Threading.Tasks;
 
 namespace ScottishGlenAssetTracking.ViewModels
 {
+    /// <summary>
+    /// Partial class for the RegisterViewModel using the ObservableObject class.
+    /// </summary>
     public partial class RegisterViewModel : ObservableObject
     {
+        // Private field for the AccountService.
         private readonly AccountService _accountService;
 
+        /// <summary>
+        /// Constructor for the RegisterViewModel class using the AccountService with dependency injection.
+        /// </summary>
+        /// <param name="accountService">AccountService from dependency injection.</param>
         public RegisterViewModel(AccountService accountService)
         {
+            // Set the account service.
             _accountService = accountService;
-
-            NewAccount = new Account();
         }
 
         // Properties.
-
         [ObservableProperty]
         private Account newAccount;
+
+        [ObservableProperty]
+        private string email;
+
+        [ObservableProperty]
+        private string password;
 
         [ObservableProperty]
         private string confirmPassword;
@@ -41,42 +54,105 @@ namespace ScottishGlenAssetTracking.ViewModels
 
         // Commands
 
+        /// <summary>
+        /// Command to register a new account.
+        /// </summary>
         [RelayCommand]
         public void Register()
         {
-            if (NewAccount.VerifyPassword(ConfirmPassword))
+            // Check if the email, password, and confirm password fields are filled in.
+            if (Email == string.Empty || Email == null ||
+                Password == string.Empty || Password == null ||
+                ConfirmPassword == string.Empty || ConfirmPassword == null)
             {
+                StatusMessage = "Please fill in all fields.";
+                StatusVisibility = Visibility.Visible;
+                return;
+            }
+
+            // Attempt to register the new account, catching any exceptions that may occur.
+            try
+            {
+                // Create a new account with the provided email and password.
+                NewAccount = new Account
+                {
+                    Email = Email,
+                    Password = Password
+                };
+
+                // Verify the password and confirm password match.
+                if (!NewAccount.VerifyPassword(ConfirmPassword))
+                {
+                    ConfirmPassword = string.Empty;
+                    SetStatusMessage("Passwords do not match.");
+                    return;
+                }
+
+                // Add the account to the database.
                 if (_accountService.AddAccount(NewAccount))
                 {
-                    // Reset the new account and confirm password properties.
-                    NewAccount = new Account();
-                    ConfirmPassword = string.Empty;
-
-                    StatusMessage = "Account added successfully";
-                    StatusVisibility = Visibility.Visible;
+                    ResetProperties();
+                    SetStatusMessage("Account added successfully.");
                 }
                 else
                 {
-                    StatusMessage = "Failed to add account";
-                    StatusVisibility = Visibility.Visible;
+                    // Do not mention if an email already exists in the database - security risk.
+                    SetStatusMessage("Account could not be added.");
                 }
             }
-            else
+            catch (ArgumentException ex)
             {
-                StatusMessage = "Passwords do not match";
-                StatusVisibility = Visibility.Visible;
+                Password = string.Empty;
+                ConfirmPassword = string.Empty;
+                SetStatusMessage(ex.Message);
+            }
+            catch (MySqlException)
+            {
+                Password = string.Empty;
+                ConfirmPassword = string.Empty;
+                SetStatusMessage("There was an issue connecting to the database. Please try again later.");
+            }
+            catch (Exception)
+            {
+                Password = string.Empty;
+                ConfirmPassword = string.Empty;
+                SetStatusMessage("An unexpected error occurred. Please try again later.");
             }
         }
 
+        /// <summary>
+        /// Command to return to the login page.
+        /// </summary>
         [RelayCommand]
         public void ReturnToLogin()
         {
-            // Reset the new account and confirm password properties.
-            NewAccount = new Account();
-            ConfirmPassword = string.Empty;
+            ResetProperties();
 
             var loginPage = App.AppHost.Services.GetRequiredService<Login>();
             MainWindow.Frame.Navigate(typeof(Login));
+        }
+
+        /// <summary>
+        /// Helper method to reset the properties of the RegisterViewModel.
+        /// </summary>
+        private void ResetProperties()
+        {
+            // Reset the new account, email, password, and status message properties.
+            NewAccount = null;
+            Email = string.Empty;
+            Password = string.Empty;
+            ConfirmPassword = string.Empty;
+            StatusMessage = string.Empty;
+        }
+
+        /// <summary>
+        /// Helper method to set the status message and make the status message visible.
+        /// </summary>
+        /// <param name="message">Message to be displayed.</param>
+        private void SetStatusMessage(string message)
+        {
+            StatusMessage = message;
+            StatusVisibility = Visibility.Visible;
         }
     }
 }
